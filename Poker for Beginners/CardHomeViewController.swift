90e34020ef33d3048cac8682170276b110b1dfd7
mpickard42 <mpickard42@gmail.com>
2017-10-26 17:25:59 -0400

    navigation between main screen and results screen

---
+++ Poker for Beginners/CardHomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CardHomeViewController.swift
+//  Poker for Beginners
+//
+//  Created by Marjorie Pickard on 10/26/17.
+//  Copyright © 2017 E&M. All rights reserved.
+//
+
+import UIKit
+
+class CardHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onResultsButtonPress(_ sender: UIButton) {
+        performSegue(withIdentifier: "resultsViewSegue", sender: self)
+    }
+
+    @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
