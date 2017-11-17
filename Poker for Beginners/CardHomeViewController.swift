f0d23df994cba54822fa6333751676ba449e1c87
mpickard42 <mpickard42@gmail.com>
2017-11-17 09:02:58 -0500

    Basic card selection

--- Poker for Beginners/CardHomeViewController.swift
+++ Poker for Beginners/CardHomeViewController.swift
@@ -9,8 +9,7 @@
 import UIKit
 
 class CardHomeViewController: UIViewController {
-    struct Data {}
-
+    var senderButtonID = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,16 +22,33 @@ class CardHomeViewController: UIViewController {
     }
     
     @IBAction func onCardPress(_ sender: UIButton) {
+        // Change to the CardSelectViewController page
         performSegue(withIdentifier: "cardSelectSegue", sender: self)
+        senderButtonID = sender
+        NSLog(String(describing: sender))
     }
     @IBAction func onResultsButtonPress(_ sender: UIButton) {
+        // Change to the ResultsViewController page
         performSegue(withIdentifier: "resultsViewSegue", sender: self)
     }
 
     @IBAction func myUnwindAction(unwindSegue: UIStoryboardSegue) {
-        
+        if let returnedData = unwindSegue.source as? CardSelectViewController{
+            //Display the returned data.  Stored as [rank, suit] both strings
+            NSLog("Rank: " + String(describing: returnedData.rankSuit[0]) + ", Suit: " + String(describing: returnedData.rankSuit[1]))
+            let rankSuit = returnedData.rankSuit
+            changeButtonPicture(suitRank: rankSuit!, senderButtonId: senderButtonID)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if (segue.identifier == "cardSelectSegue"){
+//            let vc = segue.destination as! CardSelectViewController
+//            vc.buttonID = senderButtonID
+//        }
+    }
+    
+    func changeButtonPicture (suitRank: Array<Any>, senderButtonId: UIButton){
+        senderButtonID.setImage(UIImage(named: "cards1600"), for: .normal)
     }
 }
--- Poker for Beginners/CardSelectViewController.swift
+++ Poker for Beginners/CardSelectViewController.swift
@@ -8,29 +8,66 @@
 
 import UIKit
 
-class CardSelectViewController: UIViewController {
+class CardSelectViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var rankSuitPicker: UIPickerView!
+    private let rankComponent = 0
+    private let suitComponent = 1
+    private let rankType = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    private let suitType = ["Diamond", "Heart", "Club", "Spade"]
+    var rank = 0
+    var suit = 0
+    var rankSuit : Array<Any>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        rankSuitPicker.dataSource = self
+        rankSuitPicker.delegate = self
     }
 
-      
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == rankComponent {
+            return rankType[row]
+        } else {
+            return suitType[row]
+        }
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == rankComponent {
+            return rankType.count
+        } else {
+            return suitType.count
+        }
+    }
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    @IBAction func submitButtonPress(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == rankComponent {
+            rank = row
+        }else {
+            suit = row
+        }
+    }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        rankSuit = [rankType[rank], suitType[suit]]
     }
-    */
+
 
 }
